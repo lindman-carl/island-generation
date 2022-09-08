acf755f378e12d1eed235f62cbdf6b16df0eef42
yoga-python <73431769+yoga-python@users.noreply.github.com>
2022-09-08 12:44:49 +0200

    update gitignore

---
+++ src/lib/islandGeneration.test.ts
@@ -0,0 +1,21 @@
+// import { exportedForTesting } from "./islandGeneration.js";
+
+// const { createBlankWorldMap } = exportedForTesting;
+
+// describe("createBlankWorldMap", () => {
+//   test("should return height*width number of elements", () => {
+//     const width = 20;
+//     const height = 20;
+//     const area = width * height;
+
+//     const blankWorldMap = createBlankWorldMap(width, height, 0);
+//     console.log(blankWorldMap);
+
+//     const numElements = blankWorldMap.reduce(
+//       (acc, curr) => acc + curr.length,
+//       0
+//     );
+
+//     expect(numElements).toBe(area);
+//   });
+// });
